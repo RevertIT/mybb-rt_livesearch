6d504736e6289d8ef32d8e7985e6fdcd49cea3ae
2redact <2redact@gmail.com>
2023-04-09 02:29:56 +0200

    Add initial admin lang translation

--- inc/languages/english/admin/rt_livesearch.lang.php
+++ inc/languages/english/admin/rt_livesearch.lang.php
@@ -1,1 +1,3 @@
 <?php
+$l['rt_livesearch_keypress_letter_length'] = 'Your keypress letter must not be longer than 1 character!';
+$l['rt_livesearch_keypress_letter_alphabet'] = 'Your keypress letter must be alpha character from A-Z';

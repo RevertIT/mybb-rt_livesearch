9572090af6fb71511b6ab635a3284107e6b6d13e
2redact <2redact@gmail.com>
2023-04-09 02:30:36 +0200

    Fix `namespace` and add hooks

--- inc/plugins/rt_livesearch/src/Hooks/Backend.php
+++ inc/plugins/rt_livesearch/src/Hooks/Backend.php
@@ -12,7 +12,32 @@
 
 declare(strict_types=1);
 
-namespace rt\LiveSearch;
+namespace rt\LiveSearch\Hooks;
 
 use rt\LiveSearch\Core;
 
+/**
+ * Hook: admin_config_settings_change
+ *
+ * @return void
+ */
+function admin_config_settings_change(): void
+{
+    global $mybb, $lang, $gid;
+
+    $lang->load(Core::get_plugin_info('prefix'));
+
+    if (isset($mybb->input['upsetting']['rt_livesearch_keypress_letter']))
+    {
+        if (strlen(trim_blank_chrs($mybb->input['upsetting']['rt_livesearch_keypress_letter'])) > 1)
+        {
+            flash_message($lang->rt_livesearch_keypress_letter_length, 'error');
+            admin_redirect("index.php?module=config-settings&action=change&gid=".(int)$mybb->input['gid']);
+        }
+        if (!preg_match('/[a-zA-Z]/', $mybb->input['upsetting']['rt_livesearch_keypress_letter']))
+        {
+            flash_message($lang->rt_livesearch_keypress_letter_alphabet, 'error');
+            admin_redirect("index.php?module=config-settings&action=change&gid=".(int)$mybb->input['gid']);
+        }
+    }
+}
